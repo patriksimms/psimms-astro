a69212ba4d6fcf27a7f58e48d5742c3f49b620de
Patrik Simms <patrik.simms@esome.com>
2025-09-01 21:43:19 +0200

    use Pandabox for image galleries

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,35 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/posts/` directory.
-	loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-			tags: z.array(z.enum(['engineering', 'games', 'other'])),
-			contentMaxWidth: z.boolean().default(true)
-		}),
+  // Load Markdown and MDX files in the `src/content/posts/` directory.
+  loader: glob({ base: "./src/content/posts", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+      tags: z.array(z.enum(["engineering", "games", "other"])),
+    }),
 });
 
-export const collections = { blog };
+const galleries = defineCollection({
+  loader: glob({ pattern: "*.json", base: "src/content/galleries" }),
+  schema: ({ image }) =>
+    z.object({
+      images: z.array(
+        z.object({
+          src: image(),
+          alt: z.string(),
+          title: z.string(),
+          description: z.string(),
+        }),
+      ),
+    }),
+});
+
+export const collections = { blog, galleries };
